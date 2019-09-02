365174604ed2db2567834b85d91ec3f3d9655d75
Victor Nuñez <skeiceee@gmail.com>
2019-09-02 10:33:00 -0400

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -284,7 +284,7 @@ class Kernel extends ConsoleKernel
             $revenue->file_name = $nameFile;
             $revenue->save();
 
-        })->cron('*/1 0 * *');
+        })->cron('0 4 * * *');
     }
 
     /**

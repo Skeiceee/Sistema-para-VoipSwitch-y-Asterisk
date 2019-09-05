e110bd5a45c56e14502889c1e6181afc8b7887d4
Victor Nuñez <skeiceee@gmail.com>
2019-09-05 17:06:56 -0400

    Se arreglo la forma en que esta distribuido el formulario para ser mas practico.

--- resources/views/accesscharge/index.blade.php
+++ resources/views/accesscharge/index.blade.php
@@ -5,75 +5,75 @@
         @include('layouts.menu')
         <div class="col-md-9">
             <div class="card">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div><i class="fas fa-money-check"></i><span class="font-weight-bold ml-2">Cargos de acceso</span></div>
-                    </div>
-                    <hr class="my-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                <div>
-                                    <i class="far fa-calendar-alt"></i><span class="font-weight-bold ml-2">Periodos</span>
+                <form id="accesscharge" action="" method="get">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div><i class="fas fa-money-check"></i><span class="font-weight-bold ml-2">Cargos de acceso</span></div>
+                        </div>
+                        <hr class="my-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <div>
+                                        <i class="far fa-building"></i><span class="font-weight-bold ml-2">Compañia</span>
+                                    </div>
                                 </div>
-                                <a id="add_period" href="javascript:void(0);" class="btn-sm btn-primary" data-placement="left" data-toggle="tooltip" data-original-title="Agregar nuevo periodo."><i class="fas fa-plus"></i></a>
+                                <hr class="mt-0">
+                                <div class="form-group">
+                                    <label for="">Nombre</label>
+                                    <select name="" class="form-control form-control-chosen" data-placeholder="Selecciona una compañia...">
+                                        {{-- <option value="1">Adwadaw</option>
+                                        <option value="2">Bdwadaw</option>
+                                        <option value="3">Cdwadaw</option>
+                                        <option value="4">Ddwadawdaw</option>
+                                        <option value="5">Edwadwadaw</option> --}}
+                                    </select>
+                                </div>
                             </div>
-                            <form action="" method="get">
+                        </div>
+                        <div class="card my-3">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <div><i class="far fa-calendar-alt"></i><span class="font-weight-bold ml-2">Agregar período</span></div>
+                                    <a id="add_period" href="javascript:void(0);" class="btn btn-primary" data-placement="left" data-toggle="tooltip" data-original-title="Agregar nuevo periodo."><i class="fas fa-plus"></i></a>
+                                </div>
+                                <hr class="mt-0">
                                 <div class="row form-group">
-                                    <div class="col-md-4">
+                                    <div class="col-md-6">
                                         <label for="">Fecha de inicio</label>
                                         <input id="date" type="text" data-language='es' data-min-view="months"
                                             data-view="months" data-date-format="MM - mm/yyyy" class="form-control" name="date">
                                     </div>
-                                    <div class="col-md-4">
+                                    <div class="col-md-6">
                                         <label for="">Fecha de termino</label>
                                         <input id="date" type="text" data-language='es' data-min-view="months"
                                             data-view="months" data-date-format="MM - mm/yyyy" class="form-control" name="date">
-                                    </div>
-                                    <div class="col-md">
-                                        <label for="">Tarifa</label>
-                                        <input type="number" step="0.0001" class="form-control">
                                     </div>
                                 </div>
                                 <div class="row form-group">
                                     <div class="col-md-4">
-                                        <label for="">Fecha de inicio</label>
-                                        <input id="date" type="text" data-language='es' data-min-view="months"
-                                            data-view="months" data-date-format="MM - mm/yyyy" class="form-control" name="date">
+                                        <label for="rate_period_1">Tarifa normal</label>
+                                        <input name="rate_period_1" type="number" step="0.0001" class="form-control">
                                     </div>
                                     <div class="col-md-4">
-                                        <label for="">Fecha de termino</label>
-                                        <input id="date" type="text" data-language='es' data-min-view="months"
-                                            data-view="months" data-date-format="MM - mm/yyyy" class="form-control" name="date">
+                                        <label for="rate_period_1">Tarifa reducida</label>
+                                        <input name="rate_period_1" type="number" step="0.0001" class="form-control">
                                     </div>
-                                    <div class="col-md">
-                                        <label for="">Tarifa</label>
-                                        <input type="number" step="0.0001" class="form-control">
+                                    <div class="col-md-4">
+                                        <label for="rate_period_1">Tarifa nocturna</label>
+                                        <input name="rate_period_1" type="number" step="0.0001" class="form-control">
                                     </div>
                                 </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="card mt-3">
-                        <div class="card-body">
-                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                <div>
-                                    <i class="far fa-building"></i><span class="font-weight-bold ml-2">Compañia</span>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="">Nombre</label>
-                                <select name="" class="form-control form-control-chosen">
-                                    {{-- <option value="1">Adwadaw</option>
-                                    <option value="2">Bdwadaw</option>
-                                    <option value="3">Cdwadaw</option>
-                                    <option value="4">Ddwadawdaw</option>
-                                    <option value="5">Edwadwadaw</option> --}}
-                                </select>
                             </div>
                         </div>
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <span class="text-muted">La lista de períodos esta vacia.</span>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-block btn-primary mt-3">Calcular</button>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
@@ -96,15 +96,31 @@ $(document).ready(function(){
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
-    });
+    })
     
-    $('.form-control-chosen').chosen({no_results_text: "No se ha encontrado"});
+    $('.form-control-chosen').chosen({no_results_text: "No se ha encontrado"})
 
     let addPeriod = $("#add_period")
     addPeriod.tooltip()
     addPeriod.click(function(){
-        alert('Wena!');
+        alert('Wena!')
     })
+
+    $("#accesscharge").submit(function(e) {
+        e.preventDefault()
+        var form = $(this)
+        console.log(form.serialize())
+        var url = form.attr('action')
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: form.serialize(),
+            success: function(data)
+            {
+                console.log(data)
+            }
+        });
+    });
 })
 </script>
 @endpush

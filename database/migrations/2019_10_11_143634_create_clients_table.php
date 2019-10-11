8ad6b938de5f7c68c4a0f1ee7a8c224c53cd3426
skeiceee <skeiceee@gmail.com>
2019-10-11 12:21:53 -0300

    Se modifico las migraciones.

--- database/migrations/2019_10_11_143634_create_clients_table.php
+++ database/migrations/2019_10_11_143634_create_clients_table.php
@@ -13,7 +13,7 @@ class CreateClientsTable extends Migration
      */
     public function up()
     {
-        Schema::create('clients', function (Blueprint $table) {
+        Schema::connection('mysql')->create('clients', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 50);
             $table->string('description', 500);
@@ -28,6 +28,6 @@ class CreateClientsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('clients');
+        Schema::connection('mysql')->dropIfExists('clients');
     }
 }

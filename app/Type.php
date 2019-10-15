f0427d87a0c952ee11943eab8f80b47f8027d7e0
skeiceee <skeiceee@gmail.com>
2019-10-15 09:16:31 -0300

    Configuración del modelo de tipos de numeros.

--- app/Type.php
+++ app/Type.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
+    protected $table = "types";
+    protected $primaryKey = "id";
+    protected $connection = 'mysql';
+    public $timestamps = false;
+
     public function numerations()
     {
         return $this->belongsTo(Numeration::class);

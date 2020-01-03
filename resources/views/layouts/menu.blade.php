2871214161fb50473a389082326a87037589ed31
skeiceee <skeiceee@gmail.com>
2020-01-03 17:18:29 -0300

    Avance en el modulo de Facturas.

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -27,6 +27,9 @@
                             <li class="nav-item">
                                 <a class="nav-link " href="{{ route('accesscharge.index') }}">Cargos de acceso</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="{{ route('invoices.index') }}">Facturas</a>
+                            </li>
                         </ul>
                     </div>
                     <div style="width: 150px">

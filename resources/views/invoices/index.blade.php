2ae41b1fe63f8412df61ad5b5c262a568a58d6a9
skeiceee <skeiceee@gmail.com>
2020-01-09 17:37:52 -0300

    Avance en el modulo de facturación.

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -99,7 +99,7 @@
     cursor: pointer;
 }
 .custom-radio input:checked ~ label {
-    background-color: #BA89BC;
+    background-color: #3490dc;
     color: #fff;
     box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
 }

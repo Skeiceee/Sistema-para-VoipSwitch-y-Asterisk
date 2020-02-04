175fb7265ed16d39363259fe6a84e875acd12c2b
skeiceee <skeiceee@gmail.com>
2020-02-04 10:26:05 -0300

    Arreglo de identado.

--- resources/views/rates/create.blade.php
+++ resources/views/rates/create.blade.php
@@ -22,14 +22,14 @@
                                         @endforeach
                                     </select>
                                     @error('ido')
-                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                        <span class="invalid-feedback" role="alert">{{ $message }}</span>
                                     @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="range_date">Rango de fechas</label>
                                     <input class="form-control @error('range_date') is-invalid @enderror" id="range_date" type="text" data-language='es' data-multiple-dates-separator=" al " data-date-format="dd/mm/yyyy" name="range_date"  autocomplete="off">
                                     @error('range_date')
-                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                        <span class="invalid-feedback" role="alert">{{ $message }}</span>
                                     @enderror
                                 </div>
                                 <div class="row form-group">
@@ -37,21 +37,21 @@
                                         <label for="rate_normal">Tarifa normal</label>
                                         <input class="form-control @error('rate_normal') is-invalid @enderror" name="rate_normal" type="number" step="0.0001" min="0">
                                         @error('rate_normal')
-                                        <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                            <span class="invalid-feedback" role="alert">{{ $message }}</span>
                                         @enderror
                                     </div>
                                     <div class="col-md-4">
                                         <label for="rate_reduced">Tarifa reducida</label>
                                         <input class="form-control @error('rate_reduced') is-invalid @enderror" name="rate_reduced" type="number" step="0.0001" min="0">
                                         @error('rate_reduced')
-                                        <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                            <span class="invalid-feedback" role="alert">{{ $message }}</span>
                                         @enderror
                                     </div>
                                     <div class="col-md-4">
                                         <label for="rate_night">Tarifa nocturna</label>
                                         <input class="form-control @error('rate_night') is-invalid @enderror" name="rate_night" type="number" step="0.0001" min="0">
                                         @error('rate_night')
-                                        <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                            <span class="invalid-feedback" role="alert">{{ $message }}</span>
                                         @enderror
                                     </div>
                                 </div>

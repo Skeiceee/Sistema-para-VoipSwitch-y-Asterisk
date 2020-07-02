c865eab8d80801fd721c36fb1c5525fd481f8066
Victor Nuñez <skeiceee@gmail.com>
2020-07-02 14:38:26 -0400

    Avance en el modulo de cargos de acceso entrantes.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -362,9 +362,14 @@ class Kernel extends ConsoleKernel
         // Envia el correo de consumos diarios.
         $schedule->command('email:accesscharge')->dailyAt('08:00')->timezone('America/Santiago');
 
-        $schedule->command('email:alarmsessions')->cron('0 */3 * * *')->timezone('America/Santiago');
+        // Envia el correo que avisa de las sessiones.
+        $schedule->command('email:alarmsessions')->cron('0 */1 * * *')->timezone('America/Santiago');
+
         // Crea los archivos diarios de CDRs.
         $schedule->command('daily:cdrs')->dailyAt('05:00')->timezone('America/Santiago');
+
+        // Crea los archivos mensuales de los cargos de acceso entrantes.
+        $schedule->command('monthly:inboundaccesscharge')->monthlyOn(1, '05:00')->timezone('America/Santiago');
     }
 
     /**
